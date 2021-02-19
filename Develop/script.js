2c32d17958032862e9995d0aea14491c93e257e0
Zack Hersh <zackhersh@Zacks-MBP.attlocal.net>
2021-02-18 23:10:17 -0500

    js code added to register which charecter types are allowed

--- Develop/script.js
+++ Develop/script.js
@@ -22,7 +22,7 @@ function generatePassword(){
 
   var numRange = document.getElementById("charNumRange");
   var lowerCheck = document.getElementById("lowerCaseCheck");
-  var upperCheck = document.getElementById("lowerCaseCheck");
+  var upperCheck = document.getElementById("upperCaseCheck");
   var numCheck = document.getElementById("numCheck");
   var specialCheck = document.getElementById("specialCharCheck");
 
@@ -30,5 +30,41 @@ function generatePassword(){
     document.getElementById("charNumDisplay").innerHTML = numRange.value + " charecters"
   })
 
+  var charOptions = [lowerCheck,upperCheck,numCheck,specialCheck]
 
+  var checkedCharOptions = [];
+
+  for (var i=0;i<charOptions.length;i++){
+    if(charOptions[i].checked == true){
+      checkedCharOptions.push(charOptions[i].value)
+    }
+  }
+
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  lower = lower.split('')
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  upper = upper.split('')
+  var number = "0123456789"
+  number = number.split('')
+  var special = "`~/?.>,<=+-_"
+  special = special.split('')
+
+  var passOutput = ""
+
+  for (var i=0;i<numRange.value;i++){
+    var charType = checkedCharOptions[getRandomInt(checkedCharOptions.length)];
+    charType = eval(charType)
+
+    var char = charType[getRandomInt(charType.length)]
+    passOutput += char
+
+  }
+
+  console.log(passOutput)
+  return passOutput;
+
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
