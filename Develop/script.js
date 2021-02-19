799814db921c92091dc55420cbe80603166d87dd
Zack Hersh <zackhersh@Zacks-MBP.attlocal.net>
2021-02-18 22:28:01 -0500

    js code added for display functionality

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,20 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword(){
-  generateBtn.style.display = "none";
-  
+  // generateBtn.style.display = "none";
+
+  var promptDiv = document.getElementById("generatePrompts")
+  promptDiv.style.display = "block";
+
+  var numRange = document.getElementById("charNumRange");
+  var lowerCheck = document.getElementById("lowerCaseCheck");
+  var upperCheck = document.getElementById("lowerCaseCheck");
+  var numCheck = document.getElementById("numCheck");
+  var specialCheck = document.getElementById("specialCharCheck");
+
+  numRange.addEventListener("mouseup", function(){
+    document.getElementById("charNumDisplay").innerHTML = numRange.value + " charecters"
+  })
+
+
 }
